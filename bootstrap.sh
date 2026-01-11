29446cd839c833e4cf2fae2f816af61f7ba6aac5
johnko <279736+johnko@users.noreply.github.com>
2026-01-11 16:41:03 -0500

    cp fast first

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+for i in .bash_aliases .config/opencode/opencode.jsonc ; do
+  cp "$HOME/dotfiles/$i" "$HOME/$i"
+done
+
 OPENCODE_VERSION=1.1.12 # renovate: datasource=github-releases depName=anomalyco/opencode packageName=anomalyco/opencode
 DEVCONTAINERS_VERSION=0.80.3 # renovate: datasource=github-releases depName=devcontainers/cli packageName=devcontainers/cli
 
 npm install --global opencode-ai@$OPENCODE_VERSION
 npm install --global @devcontainers/cli@$DEVCONTAINERS_VERSION
-
-for i in .bash_aliases .config/opencode/opencode.jsonc ; do
-  cp "$HOME/dotfiles/$i" "$HOME/$i"
-done
