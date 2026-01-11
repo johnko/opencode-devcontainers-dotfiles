e257f5a4b81586f230f6e162b50f7532f64d7ab6
johnko <279736+johnko@users.noreply.github.com>
2026-01-11 16:27:45 -0500

    cp

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,7 @@ DEVCONTAINERS_VERSION=0.80.3 # renovate: datasource=github-releases depName=devc
 
 npm install --global opencode-ai@$OPENCODE_VERSION
 npm install --global @devcontainers/cli@$DEVCONTAINERS_VERSION
+
+for i in .bash_aliases .config/opencode/opencode.jsonc ; do
+  cp "$i" "$HOME/$i"
+done
