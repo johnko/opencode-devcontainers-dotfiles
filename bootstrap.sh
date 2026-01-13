b56504ab8745945c988ff76596148268efa40565
johnko <279736+johnko@users.noreply.github.com>
2026-01-13 01:16:49 -0500

    create directory and copy if not exist

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
-for i in .bash_aliases .config/opencode/opencode.jsonc; do
-  mkdir -p "$HOME/$(dirname "$i")"
-  cp "$HOME/dotfiles/$i" "$HOME/$i"
+for i in $(find "$HOME/dotfiles" -type f -not -path "*/.git/*" -not -path "*/.devcontainer/*" -not -path "*/bootstrap.sh" | sed "s,$HOME/dotfiles/,,g"); do
+  if [[ "$(dirname "$i")" != "." && "$(dirname "$i")" != "/" && "$(dirname "$i")" != "" ]]; then
+    if [[ ! -e "$HOME/$(dirname "$i")" ]]; then
+    mkdir -p "$HOME/$(dirname "$i")"
+    fi
+  fi
+  if [[ ! -e "$HOME/$i" ]]; then
+    cp -v "$HOME/dotfiles/$i" "$HOME/$i"
+  fi
 done
+
+set -x
 
 OPENCODE_VERSION=1.1.12      # renovate: datasource=github-releases depName=anomalyco/opencode packageName=anomalyco/opencode
 DEVCONTAINERS_VERSION=0.80.3 # renovate: datasource=github-releases depName=devcontainers/cli packageName=devcontainers/cli
