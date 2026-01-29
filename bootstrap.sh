96c69c5954fe95cb84856c0503bc5136e974d722
johnko <279736+johnko@users.noreply.github.com>
2026-01-29 02:42:48 -0500

    backport

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -uxo pipefail
 
 for i in $(find "$HOME/dotfiles" -type f -not -path "*/.git/*" -not -path "*/.devcontainer/*" -not -path "*/bootstrap.sh" | sed "s,$HOME/dotfiles/,,g"); do
   if [[ "$(dirname "$i")" != "." && "$(dirname "$i")" != "/" && "$(dirname "$i")" != "" ]]; then
     if [[ ! -e "$HOME/$(dirname "$i")" ]]; then
-    mkdir -p "$HOME/$(dirname "$i")"
+      mkdir -p "$HOME/$(dirname "$i")"
     fi
   fi
   if [[ ! -e "$HOME/$i" ]]; then
-    cp -v "$HOME/dotfiles/$i" "$HOME/$i"
+    cp -a -v "$HOME/dotfiles/$i" "$HOME/$i"
   fi
 done
-
-set -x
-set +e
